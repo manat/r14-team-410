1d0d79b925696edee8ce7db1dcc649f029f75d4b
Manat Lertpeetipat <mlertpee@gmail.com>
2014-10-19 12:37:18 +0700

    Defined roles and abilities for #6 user roles.
    * Used cancancan to manage this
    * 3 roles are created: admin, quizmaster, and 
    candidate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019040927) do
+ActiveRecord::Schema.define(version: 20141019043208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20141019040927) do
     t.string   "uid"
     t.string   "username"
     t.string   "name"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe User do
+  describe 'user with quizmaster role' do
+    before do
+      @user = users(:quizmaster)
+      @another_user = users(:admin)
+      @ability = Ability.new(@user)
+    end
+
+    it 'can only crud exams which he/she owns' do
+      @ability.can?(:create, Exam.new(user: @user)).must_equal true
+      @ability.cannot?(:create, Exam.new(user: @another_user)).must_equal true
+    end
+
+    it 'can only crud questions which he/she owns' do
+      exam = Exam.new(user: @user)
+      another_exam = Exam.new(user: @another_user)
+
+      @ability.can?(:create, Question.new(exam: exam)).must_equal true
+      @ability.cannot?(:create, Question.new(exam: another_exam)).must_equal true
+    end
+  end
+
+  describe 'user with admin role' do
+    before do
+      @user = users(:admin) 
+      @ability = Ability.new(@user)
+    end
+
+    it 'can manage everything' do
+      @ability.can?(:create, Exam.new(user: @user)).must_equal true
+      @ability.can?(:create, Question.new).must_equal true
+    end
+  end
 end
